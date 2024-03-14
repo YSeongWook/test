ea3221ff38f185fcceafcd71e1b46f683570badf
K-Byeonggon <71167796+K-Byeonggon@users.noreply.github.com>
2024-03-14 15:10:10 +0900

    Add files via upload

---
+++ CSharp_20240314_GitHubTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp_20240314_GitHubTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub!");
+        }
+    }
+}
